27fdeeeed21e57cc059d4c54febbe5102ae6cb7b
Samet Uslu <samet_u@hotmail.de>
2021-10-23 12:59:44 +0200

    Add card and a little bit style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import Example from './components/Example'
+import Card from './components/Card'
 import { getBySelector } from './lib/dom'
 import './styles/index.css'
 
-const example = Example('Vite Starter')
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=100'
 
-getBySelector('#app').append(example)
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    data.results.forEach(({ url }) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(pokemon => renderCards(pokemon))
+    })
+  })
+
+function renderCards(data) {
+  const container = getBySelector('#pokedex')
+  const card = Card({
+    name: data.name,
+    image: data.sprites.front_default,
+    id: data.id,
+    height: data.height,
+  })
+  container.append(card)
+}
